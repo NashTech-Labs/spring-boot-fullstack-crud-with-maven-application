3de4d7f136f2767ee1eb3358cc1e18c91b183c57
Shashikant-15 <shashikant.tanti@knoldus.com>
2023-01-16 15:40:16 +0530

    final commit

---
+++ backend-spring-boot-react-crud-full-stack-with-maven/src/test/java/com/knoldus/fullstack/springboot/maven/crud/SpringBootReactCrudFullStackWithMavenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.knoldus.fullstack.springboot.maven.crud;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes={SpringBootFullStackCrudFullStackWithMavenApplication.class})
+public class SpringBootReactCrudFullStackWithMavenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
